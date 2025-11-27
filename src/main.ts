70b891de782cde41217abd42ab4005b7253f8982
Alex <alexyakimenkojr@gmail.com>
2025-11-27 19:30:46 +0200

    feature/templates: add ejs as template engine

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import express from 'express'
 import config from '@/config/config'
 import sequelize from '@/models'
+import * as path from 'node:path'
 
 const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.render('index')
+})
 
 sequelize.sync({ alter: true }).then(async () => {
   app.listen(config.app.port, () => {
