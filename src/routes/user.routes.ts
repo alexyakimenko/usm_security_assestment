0232acaa1f1d46731d5dc673a39737d571637d6b
Alex <alexyakimenkojr@gmail.com>
2025-11-28 21:33:27 +0200

    faeture/user: add user profile

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,6 +3,8 @@ import * as controller from '@/controllers/user.controller'
 
 const router = Router()
 
-router.get('/profile', controller.profile)
+router.get('/profile', controller.myProfile)
+router.get('/profile/:username', controller.profile)
+router.post('/remove/:id', controller.remove)
 
 export default router
